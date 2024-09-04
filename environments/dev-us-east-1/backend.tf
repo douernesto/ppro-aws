ee3f80234983d43af834dabd6a798166dbb18d36
douernesto <ernesto.gonzalez@digitalonus.com>
2024-09-04 17:03:44 -0600

    Final code

--- environments/dev-us-east-1/backend.tf
+++ environments/dev-us-east-1/backend.tf
@@ -1,3 +1,9 @@
+# ┌─────────────────────────────────────────────────────────────────────────────────────────────────┐
+# │                                   IMPORTANT NOTICE                                              |
+# │ Not setting a remote backend on purpose to avoid cost as this only a demo.                      |
+# │ Terraform code can tested and applied by cloning this repo and run commands locally.            |
+# └─────────────────────────────────────────────────────────────────────────────────────────────────┘
+
 # terraform {
 #   backend "s3" {
 #     bucket         = "terraform"
